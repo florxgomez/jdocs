0ddc2d41ddc0bc45eddf3d05f0933e3b1244e7e4
Florencia Gomez <florxgomez@gmail.com>
2021-11-21 15:01:11 -0300

    safely handlingunstrusted code execution

--- src/index.tsx
+++ src/index.tsx
@@ -6,8 +6,8 @@ import { fetchPlugin } from './plugins/fetch-plugin';
 
 const App = () => {
   const ref = useRef<any>();
+  const iframe = useRef<any>();
   const [input, setInput] = useState('');
-  const [code, setCode] = useState('');
 
   const startService = async () => {
     //this gives us a reference to the esbuild service and we can use it to make
@@ -27,6 +27,8 @@ const App = () => {
       return;
     }
 
+    iframe.current.srcdoc = html;
+
     //transpiling/bundling process
     const result = await ref.current.build({
       entryPoints: ['index.js'], //bundle this file first
@@ -38,8 +40,28 @@ const App = () => {
         global: 'window',
       },
     });
-    setCode(result.outputFiles[0].text);
+    //setCode(result.outputFiles[0].text);
+    iframe.current.contentWindow.postMessage(result.outputFiles[0].text, '*');
   };
+
+  const html = `
+    <html>
+      <head></head>
+      <body>
+        <div id="root"></div>
+        <script>
+          window.addEventListener('message', (event) => {
+            try {
+              eval(event.data);
+            } catch (err) {
+              const root = document.querySelector("#root");
+              root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>';
+              console.error(err);
+            }
+          }, false)
+        </script>
+      </body>
+    </html>`;
 
   return (
     <div>
@@ -50,7 +72,12 @@ const App = () => {
       <div>
         <button onClick={onClick}>Submit</button>
       </div>
-      <pre>{code}</pre>
+      <iframe
+        title="preview"
+        ref={iframe}
+        sandbox="allow-scripts"
+        srcDoc={html}
+      />
     </div>
   );
 };
