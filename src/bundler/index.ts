677945e1225830a9a4833777b737b217408a4a73
Florencia Gomez <florxgomez@gmail.com>
2021-11-22 03:09:56 -0300

    Adding error handling

--- src/bundler/index.ts
+++ src/bundler/index.ts
@@ -12,18 +12,28 @@ const bundle = async (rawCode: string) => {
     });
   }
 
-  const result = await service.build({
-    entryPoints: ['index.js'], //bundle this file first
-    bundle: true,
-    write: false,
-    plugins: [unpkgPathPlugin(), fetchPlugin(rawCode)],
-    define: {
-      'process.env.NODE_ENV': '"production"',
-      global: 'window',
-    },
-  });
-
-  return result.outputFiles[0].text;
+  try {
+    const result = await service.build({
+      entryPoints: ['index.js'], //bundle this file first
+      bundle: true,
+      write: false,
+      plugins: [unpkgPathPlugin(), fetchPlugin(rawCode)],
+      define: {
+        'process.env.NODE_ENV': '"production"',
+        global: 'window',
+      },
+    });
+    return {
+      code: result.outputFiles[0].text,
+      err: '',
+    };
+  } catch (err) {
+    return {
+      code: '',
+      //@ts-ignore
+      err: err.message,
+    };
+  }
 };
 
 export default bundle;
--- src/components/CodeCell.tsx
+++ src/components/CodeCell.tsx
@@ -7,11 +7,13 @@ import Resizable from './Resizable';
 const CodeCell = () => {
   const [code, setCode] = useState('');
   const [input, setInput] = useState('');
+  const [err, setErr] = useState('');
 
   useEffect(() => {
     const timer = setTimeout(async () => {
       const output = await bundle(input);
-      setCode(output);
+      setCode(output.code);
+      setErr(output.err);
     }, 750);
 
     return () => {
@@ -28,7 +30,7 @@ const CodeCell = () => {
             onChange={(value) => setInput(value)}
           />
         </Resizable>
-        <Preview code={code} />
+        <Preview code={code} err={err} />
       </div>
     </Resizable>
   );
